24cf02de24fffc4a08684149329f1ad0657ba56d
Artur Carvalho <arturcarvalho0507@gmail.com>
2024-07-19 19:14:39 +0100

    added init_stacks function that initializes the stack a and b and reads arguments and stores them in stack a in the correct order. 1st arg is the top of the stack(tail)

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,35 +6,38 @@
 /*   By: artuda-s < artuda-s@student.42porto.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/17 20:17:19 by artuda-s          #+#    #+#             */
-/*   Updated: 2024/07/19 19:03:54 by artuda-s         ###   ########.fr       */
+/*   Updated: 2024/07/19 19:13:28 by artuda-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/push_swap.h"
 
+void	init_stacks(t_stack *stack_a, t_stack *stack_b, int ac, char ** av)
+{
+	int i;
+
+	stack_a->head = NULL;
+	stack_a->tail = NULL;
+	stack_a->size = 0;
+	stack_b->head = NULL;
+	stack_b->tail = NULL;
+	stack_b->size = 0;
+	i = 1;
+	while (i < ac)
+		insert_at_head(&stack_a, ft_atoi(av[i++]));
+	return ;
+}
 int	main(int ac, char **av)
 {
-	check_arguments(ac, av);
-	printf("Ok!\n");
-
 	t_stack stack_a;
 	t_stack stack_b;
+	check_arguments(ac, av);
+	printf("Ok args!\n");
 
-	stack_a.head = NULL;
-	stack_a.tail = NULL;
-	stack_a.size = 0;
-	stack_b.head = NULL;
-	stack_b.tail = NULL;
-	stack_b.size = 0;
+	init_stacks(&stack_a, &stack_b, ac, av);
+	printf("list read from args:\n");
+	print_list(stack_a.head); //todo deleteme
 
-	int i = 1;
-	while (i < ac)
-	{
-		insert_at_head(&stack_a, ft_atoi(av[i]));
-		i++;
-	}
-
-	print_list(stack_a.head);
 
 
 	free_list(&stack_a.head);
